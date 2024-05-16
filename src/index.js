7234f04fb3e188ef7a4f7dd963fe6716b9606c20
Mekhi-M <mekhim0731@gmail.com>
2024-05-16 18:45:40 -0400

    finished

--- src/index.js
+++ src/index.js
@@ -2,5 +2,32 @@
  *
  */
 
-const handleSubmit = () => {
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const form = event.target;
+
+  const username = form.username.value;
+  const codingLevel = form.codingLevel.value;
+  const location = form.location.value;
+  const didLikeAssignment = form.didLikeAssignment.checked;
+
+  document.querySelector("#results-username").textContent = username;
+  document.querySelector("#results-coding-level").textContent = codingLevel;
+  document.querySelector("#results-location").textContent = location;
+  didLikeAssignment
+    ? (document.querySelector("#results-did-like-assignment").textContent =
+        "Yes")
+    : (document.querySelector("#results-did-like-assignment").textContent =
+        "No");
+
+  form.reset();
 };
+
+const main = () => {
+  document
+    .querySelector("#new-user-form")
+    .addEventListener("submit", handleSubmit);
+};
+
+main();
